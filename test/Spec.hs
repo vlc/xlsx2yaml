4798f15b38f2c1966fc67102ad3e21f3ed28578b
Nick Partridge <nkpart@gmail.com>
2016-10-04 15:18:22 +1000

    Adds extra test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,34 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import qualified Lib            as Lib
+import qualified Data.Aeson as JSON
+import Control.Monad (unless)
+import Data.Aeson ((.=))
 
 main :: IO ()
 main = do
   Just _ <-
-    Lib.readSheet 4 10 "test/sample-inputs/Design Specification.xlsx" "MASTER"
+    Lib.readSheet (10 :: Int) (4 :: Int) "test/sample-inputs/Design Specification.xlsx" "MASTER"
+
+  Just brews <-
+    Lib.readSheet (10 :: Int) (2 :: Int) "test/sample-inputs/Brews.xlsx" "Brews"
+
+  let expected = JSON.object [
+                    ("Brews" .=
+                    [JSON.object ["style" .= JSON.String "ipa",
+                                  "rating" .= JSON.Number 8.0,
+                                  "name" .= JSON.String "hop hog"],
+                      JSON.object ["style" .= JSON.String "american amber",
+                                  "rating" .= JSON.Number 9.0,
+                                  "name" .= JSON.String "fanta pants"]]
+                    )
+                    ]
+
+  unless (brews == expected) $
+   do putStrLn "Bad interpretation of Brews.xlsx"
+      putStrLn "Expected:"
+      print expected
+      putStrLn "Actual:"
+      print brews
+      fail "TEST FAIL"
   pure ()
